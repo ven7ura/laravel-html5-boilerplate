d57f6a30ecceee169597af8d672714802956c6d1
J. Ventura <jventura00@gmail.com>
2019-06-14 00:35:12 -0600

    Fixes a bug during tests failing due to using a helper function in the configuration file

--- src/views/common/header.blade.php
+++ src/views/common/header.blade.php
@@ -1,6 +1,7 @@
 <section style="margin-top: 10px;">
-        <p>
-            <h1>Header content...</h1>
-            A professional front-end template for building fast, robust, and adaptable web apps or sites using Blade Template and Laravel.
-        </p>
+    <p>
+        <h1>Header content...</h1>
+        A professional front-end template for building fast, robust, and adaptable web apps or sites using Blade
+        Template and Laravel.
+    </p>
 </section>
--- src/views/common/navigation.blade.php
+++ src/views/common/navigation.blade.php
@@ -1,3 +1,4 @@
 <nav style="background-color: #333; overflow: hidden;">
-  <a class="navbar-brand" href="#" style="float: left; display: block; color: #f2f2f2; text-align: center; padding: 14px 16px; text-decoration: none; font-size:  17px;">{{ config('app.name') }}</a>
+  <a class="navbar-brand" href="#"
+    style="float: left; display: block; color: #f2f2f2; text-align: center; padding: 14px 16px; text-decoration: none; font-size:  17px;">{{ config('app.name') }}</a>
 </nav>
