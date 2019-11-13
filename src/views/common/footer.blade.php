355f801f9d0ba2c9143fd1ea03ff291330149ac6
Hichem KHIAL <khial.ali.hichem@gmail.com>
2019-11-13 10:24:33 -0500

    dynamic year in copyright

--- src/views/common/footer.blade.php
+++ src/views/common/footer.blade.php
@@ -1,5 +1,5 @@
 <footer style="position: absolute; bottom: 0; width: 100%; height: 60px; line-height: 60px; background-color: #f5f5f5">
     <div class="container">
-        &copy; 2019 All rights reserved.
+        &copy; {{ now()->year }} All rights reserved.
     </div>
 </footer>
